1c873a98990845647c85fae162a48bceeff85312
Deep841 <kumardeep2003@gmail.com>
2025-07-16 11:48:50 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Deep',
+    author_email= 'kumardeep2003@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
